53348ce2b48132ae82fba6bc218d61e1e970a7bb
Luis Kellermann <lekellerm95@gmail.com>
2024-03-28 19:01:49 +0000

    Add JWT strategy dependency function

---
+++ backend/src/auth/jwt.py
@@ -0,0 +1,19 @@
+"""Module containing the JWT token generation and verification functions."""
+
+from fastapi_users.authentication import JWTStrategy
+from .config import SECRET_KEY, JWT_ALGORITHM, JWT_LIFETIME_SECONDS
+
+
+def get_jwt_strategy() -> JWTStrategy:  # type: ignore
+    """
+    Utility function to create a JWTStrategy object with the correct configuration, to be used as a dependency callable
+    with the fastapi_users AuthenticationBackend class
+
+    Returns:
+        JWTStrategy: A JWTStrategy object with the correct auth configuration
+    """
+    return JWTStrategy(
+        secret=SECRET_KEY,
+        lifetime_seconds=JWT_LIFETIME_SECONDS,
+        algorithm=JWT_ALGORITHM,
+    )  # type: ignore
