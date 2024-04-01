e322d1fc9fd36ae81834ac38ce701f8b196652ff
Luis Kellermann <lekellerm95@gmail.com>
2024-04-01 01:15:44 +0000

    Refactor imports and get_user_manager dependency function params

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -4,9 +4,9 @@ from typing import Annotated
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..database import get_async_session
-from .models import User
-from .service import UserManager
+from src.database import get_async_session
+from src.auth.models import User
+from src.auth.service import UserManager
 
 
 async def get_user_db(  # type: ignore
@@ -19,14 +19,12 @@ async def get_user_db(  # type: ignore
         session (AsyncSession, optional): session instance, injected by the get_async_session dependency. Defaults to Depends(get_async_session).
 
     Yields:
-        SQLAlchemyUserDatabase: User database adapter for SQLAlchemy, provided by fastapi_users
+        SQLAlchemyUserDatabase: UserDatabase instance for SQLAlchemy, adapter provided by fastapi_users
     """
     yield SQLAlchemyUserDatabase(async_session, User)
 
 
-async def get_user_manager(
-    user_db: Annotated[SQLAlchemyUserDatabase[User], Depends(get_user_db)],  # type: ignore
-):
+async def get_user_manager(user_db=Depends(get_user_db)):  # type: ignore
     """
     Dependency that creates the UserManager instance for the User model, which will later be injecteed at runtime
 
