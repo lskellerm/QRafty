82abd0c25e4f5bc8e8fc8b345ceb665931a02aa7
Luis Kellermann <lekellerm95@gmail.com>
2024-04-03 00:39:49 +0000

    Fixed typo in docstring

--- backend/src/database.py
+++ backend/src/database.py
@@ -26,13 +26,12 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     """
-        Dependency that creates a new session and then yields it
+    Dependency that creates a new session and then yields it
 
-        Returns:
-            AsyncGenerator[AsyncSession, None]: A fresh SQLAlchemy session to interact with the database
-    a
-        Yields:
-            Iterator[AsyncGenerator[AsyncSession, None]]: A new async session
+    Returns:
+        AsyncGenerator[AsyncSession, None]: A fresh SQLAlchemy session to interact with the database.
+    Yields:
+        Iterator[AsyncGenerator[AsyncSession, None]]: A new async session
     """
     async with async_session_maker() as session:
         yield session
