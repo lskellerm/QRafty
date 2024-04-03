e1f6b363153010c2abbdc8bb6b830e876835b322
Luis Kellermann <lekellerm95@gmail.com>
2024-04-03 00:35:06 +0000

    Implement error handling for non-unique username and password complexity contstraint, closes #25

--- backend/src/auth/service.py
+++ backend/src/auth/service.py
@@ -2,8 +2,14 @@
 
 import uuid
 import re
-from typing import Union
+from typing import Annotated, Union, Optional
+
+from fastapi import Depends, HTTPException, Request
 from fastapi_users import BaseUserManager, UUIDIDMixin, InvalidPasswordException
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from fastapi_users import schemas  # imported for type hinting
+
+
 from src.auth.models import User
 from src.auth.schemas import UserCreate
 from src.auth.config import SECRET_KEY
@@ -22,8 +28,6 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     reset_pasword_token_secret = SECRET_KEY
     verification_token_secret = SECRET_KEY
 
-    # TODO: Implement custom business logic for the UserManager
-
     async def validate_password(  # type: ignore
         self,
         password: str,
@@ -36,6 +40,8 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
             password (str): Password to validate
             user (User): User object to validate the password against
 
+        Returns: None, if the password is valid
+
         Raises:
             InvalidPasswordException: If the password is invalid.
         """
@@ -47,4 +53,30 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
                 reason="Password should be at least 8 characters long, contain at least one uppercase letter, one digit and one special character"
             )
 
-        return await super().validate_password(password, user)
+    async def create(
+        self,
+        user_create: Annotated[schemas.UC, "UserCreate schema for FastAPIUsers"],
+        safe: bool = False,
+        request: Optional[Request] = None,
+    ) -> User:
+        """
+        Create a new user in the database.
+
+        Args:
+            user_create (UserCreate): UserCreate object with the user's data
+            safe (bool, optional): Whether to create the user safely. Defaults to False.
+
+        Returns:
+            User: User object for the created user
+        """
+        existing_user = await self.user_db.get_by_username(user_create.username)  # type: ignore
+
+        if existing_user:
+            raise HTTPException(
+                status_code=400,
+                detail="Username already exists",
+            )
+
+        return await super().create(user_create, safe=safe, request=request)
+
+    # TODO: Implement the remainder of custom User business logic for the UserManager
