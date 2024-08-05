ebaabb75ec91c19307f9dcb8955bb2eb21c068f7
Luis Kellermann <lekellerm95@gmail.com>
2024-08-05 04:51:56 +0000

    refactor: Add base invalid password messages as fixtures
    Add two  fixtures to provide  invalid password messages for usage across different tests.
    
    Eliminated redundantly hardcoding expected error messages in testst

--- backend/tests/auth/conftest.py
+++ backend/tests/auth/conftest.py
@@ -14,3 +14,21 @@ def base_registration_payload():
         "password": "TestPassword1!",
         "name": "Test User",
     }
+
+
+@pytest.fixture(scope="module")
+def invalid_password_message():
+    """
+    Provides a base invalid password message for usage across different tests, removing the need to hardcode the message in each test
+    """
+    return "Password should be at least 8 characters long, contain at least one uppercase letter, one digit and one special character"
+
+
+@pytest.fixture(scope="module")
+def invalid_password_contains_pii_info_message():
+    """
+    Provides a base invalid password message for usage across different tests, removing the need to hardcode the message in each test
+    """
+    return (
+        "Password should not contain your email, username or name for security reasons"
+    )
