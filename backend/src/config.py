efcfa3159f350ec6a30cdc6aa4d39bbc2ee1385e
Luis Kellermann <lekellerm95@gmail.com>
2024-08-05 21:46:22 -0600

    build(config): Update DEV_DATABASE_URL and SECRET_KEY to be optional
    Make DEV_DATABASE_URL and SECRET_KEY optional in the Settings class to allow for flexibility in running CI workflows, where only certain environment variables are set during the build step.

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,12 +1,13 @@
 """Global Settings module for the application."""
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from Typing import Optional
 
 
 class Settings(BaseSettings):
-    DEV_DATABASE_URL: str
+    DEV_DATABASE_URL: Optional[str] = None
     TEST_DATABASE_URL: str
-    SECRET_KEY: str
+    SECRET_KEY: Optional[str] = None
     ENVIRONMENT: str
     SHOW_DOCS_ENVIRONMENTS: tuple[str, str, str] = ("development", "staging", "testing")
     app_name: str = "QRafty API"
