483ecbf4efeb88009c9a8ad596bc1bdcab04827f
Luis Kellermann <lekellerm95@gmail.com>
2024-08-14 03:11:13 +0000

    feat(pages): Add authentication layout and registration page for User Registration feature

---
+++ frontend/pages/register.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { definePageMeta } from '#imports';
+definePageMeta({
+  layout: 'auth'
+});
+</script>
+
+<template>
+  <div>
+    <div class="flex-shrink-0 self-stretch">
+      <NuxtImg src="/img/heros/mobile-form-hero.svg" sizes="100vw" />
+    </div>
+    <div class="flex flex-col items-center gap-11 shrink-0 py-10 px-5">
+      <AuthNav />
+    </div>
+    <UserRegistrationForm />
+  </div>
+</template>
