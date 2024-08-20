89176c78df19bafdfa17aa62597c23a44fcd7059
Luis Kellermann <lekellerm95@gmail.com>
2024-08-20 05:36:22 +0000

    refactor(config): Update SECRET_KEY type to SecretType in config.py for clearer type annotation and variable type clarity

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,13 +1,14 @@
 """Global Settings module for the application."""
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from fastapi_users.jwt import SecretType
 from typing import Optional
 
 
 class Settings(BaseSettings):
     DEV_DATABASE_URL: Optional[str] = None
     TEST_DATABASE_URL: str
-    SECRET_KEY: Optional[str] = None
+    SECRET_KEY: SecretType
     ENVIRONMENT: str
     SHOW_DOCS_ENVIRONMENTS: tuple[str, str, str] = ("development", "staging", "testing")
     app_name: str = "QRafty API"
