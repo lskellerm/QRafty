45aa80cb86a1cbf8bc7afcbc3fe14d6bd91f1f2b
Luis Kellermann <lekellerm95@gmail.com>
2024-08-22 06:01:09 +0000

    feat: Add dynamic lazy loading for hero images on registration page
    The hero image displayed now depends on the screen width, showing a different image for desktop viewports and mobile devices.

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,18 +1,38 @@
-<script lang="ts">
+<script setup lang="ts">
 import { definePageMeta } from '#imports';
+import { useWindowSize } from '@vueuse/core';
+
+// Defined the page metadata for the Register page
 definePageMeta({
   layout: 'auth'
 });
+
+// Extract the width from the window size using the useWindowSize composable
+const { width } = useWindowSize();
+
+// Computed property to determine if the screen width is less than 768px, returns true if it is, false otherwise
+const isMobile = computed(() => width.value <= 768);
+
+console.log(isMobile.value, width.value);
 </script>
 
 <template>
   <div>
     <div class="flex-shrink-0 self-stretch">
-      <NuxtImg src="/img/heros/mobile-form-hero.svg" sizes="100vw" />
+      <NuxtImg
+        v-if="!isMobile"
+        src="/img/heros/desktop-register-hero.svg"
+        sizes="100vw"
+      />
+      <NuxtImg
+        v-if="isMobile"
+        src="/img/heros/mobile-form-hero.svg"
+        sizes="100vw"
+      />
     </div>
     <div class="flex flex-col items-center gap-11 shrink-0 py-10 px-5">
       <AuthNav />
+      <UserRegistrationForm />
     </div>
-    <UserRegistrationForm />
   </div>
 </template>
