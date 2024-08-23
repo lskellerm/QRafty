7c6bb82d6238deb3bf8c1f02a7f700a6e4d0e30f
Luis Kellermann <lekellerm95@gmail.com>
2024-08-23 01:03:42 +0000

    chore: Update auth layout to remove unnecessary container class

--- frontend/layouts/auth.vue
+++ frontend/layouts/auth.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Custom minimal layout for authentication routes (/register, /login) -->
-  <div class="flex flex-col items-start shrink-0 lg:container">
+  <div class="flex flex-col items-start">
     <slot />
   </div>
 </template>
