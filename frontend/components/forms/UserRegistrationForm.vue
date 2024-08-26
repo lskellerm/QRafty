ed217e81f8abad05758a57ff66ec655b02b93636
Luis Kellermann <lekellerm95@gmail.com>
2024-08-26 22:38:23 +0000

    feat(registration-validation): Add client-side form validation for User Registration flow
      - Implemented client-side validations using Vee-Validate with Zod integration .
      - Validations cover are required fields, ensuring to provide error feedback for users.
      - Included password complexity requirements.
      - Implemented mechanism to prevent duplicate submissions, disabling button while showing loading spinner.

--- frontend/components/forms/UserRegistrationForm.vue
+++ frontend/components/forms/UserRegistrationForm.vue
@@ -1,8 +1,114 @@
 <script setup lang="ts">
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import { vAutoAnimate } from '@formkit/auto-animate';
+import * as z from 'zod';
+
+// Regex pattern search for a password with at least 1 uppercase letter, 1 digit, 1 special character and a minimum length of 8 characters
+const PATTERN = /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])(?!.*\s).{8,}$/;
+
+// Define the User Registration form schema using vee-validate/zod to integrate with Zod schema validation
+const formSchema = toTypedSchema(
+  z
+    .object({
+      name: z
+        .string({
+          required_error: 'Name is required'
+        })
+        .min(1, {
+          message: 'Name is required'
+        })
+        .min(2, {
+          message: 'Name must be at least 2 characters long'
+        })
+        .max(30, {
+          message: 'Name must be less than 30 characters long'
+        }),
+      username: z
+        .string({
+          required_error: 'Username is required'
+        })
+        .min(1, {
+          message: 'Username is required'
+        })
+        .min(2, {
+          message: 'Username must be at least 2 characters long'
+        })
+        .max(30, {
+          message: 'Username must be less than 30 characters long'
+        }),
+      email: z
+        .string()
+        .min(1, {
+          message: 'Email is required'
+        })
+        .email({ message: 'Invalid email address' }),
+      password: z
+        .string({
+          required_error: 'Password is required'
+        })
+        .min(8, {
+          message: 'Password must be at least 8 characters long'
+        })
+        .min(1, {
+          message: 'Password is required'
+        })
+        .regex(PATTERN, {
+          message:
+            'Password must contain at least 1 uppercase letter, 1 digit, 1 special character and no spaces'
+        }),
+      agreeToTerms: z
+        .boolean({
+          required_error: 'You must agree to the terms and conditions'
+        })
+        .refine(
+          (value) => {
+            return value === true;
+          },
+          {
+            message: 'You must agree to the terms and conditions'
+          }
+        )
+        .default(false)
+    })
+    .required()
+);
+
+/* Extract the handleSubmit, isSubmitting state, from the useForm composable to handle form submission and validation,
+   while also providing the initial values for the form fields, set to undefined to catch fields which are not filled in.
+*/
+const { handleSubmit, isSubmitting } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    name: undefined,
+    username: undefined,
+    email: undefined,
+    password: undefined,
+    agreeToTerms: undefined
+  }
+});
+
+const submitForm = handleSubmit(async (values) => {
+  /**
+   * Submission handler for the User Registration form, using the handleSubmit function from the Vee-validate useForm composable
+   * @param {Object} values - The user submitted form values
+   */
+  console.log('Form submitted with values: ', values);
+  // TODO: Submit the form data to the serve using actual API call
+  // try {
+  //   // Simulate a delay to show the loading state
+  //   await new Promise((resolve) => setTimeout(resolve, 2000));
+  //   console.log('Form submitted successfully');
+  //   handleReset();
+  // } catch (error) {
+  //   console.error('Error submitting form: ', error);
+  // }
+});
+
 // Extract the button size based on the screen width using the useButtonSize composable
 const { buttonSize } = useButtonSize();
 
-// State to track the
+// State to track the visibility of the password field
 const isPasswordVisible = ref(false);
 
 const togglePasswordVisibility = () => {
@@ -34,12 +140,13 @@ const togglePasswordVisibility = () => {
       <form
         novalidate
         class="flex flex-col justify-center items-center px-3 py-0 gap-4"
+        @submit="submitForm"
       >
         <FormField v-slot="{ componentField }" name="name">
-          <FormItem class="w-11/12">
+          <FormItem v-auto-animate class="w-11/12">
             <FormLabel for="name" class="font-sans text-text text-sm"
-              >Name</FormLabel
-            >
+              >Name
+            </FormLabel>
             <FormControl>
               <Input
                 class="font-sans text-sm placeholder:text-slate-400"
@@ -49,10 +156,11 @@ const togglePasswordVisibility = () => {
                 name="name"
               />
             </FormControl>
+            <FormMessage />
           </FormItem>
         </FormField>
         <FormField v-slot="{ componentField }" name="username">
-          <FormItem class="w-11/12">
+          <FormItem v-auto-animate class="w-11/12">
             <FormLabel for="username" class="font-sans text-text text-sm"
               >Username</FormLabel
             >
@@ -65,10 +173,11 @@ const togglePasswordVisibility = () => {
                 name="username"
               />
             </FormControl>
+            <FormMessage />
           </FormItem>
         </FormField>
         <FormField v-slot="{ componentField }" name="email">
-          <FormItem class="w-11/12">
+          <FormItem v-auto-animate class="w-11/12">
             <FormLabel for="email" class="font-sans text-text text-sm"
               >Email</FormLabel
             >
@@ -78,7 +187,7 @@ const togglePasswordVisibility = () => {
                   class="font-sans text-sm pl-8 placeholder:text-slate-400"
                   type="email"
                   v-bind="componentField"
-                  placeholder="m@example.com"
+                  placeholder="email@example.com"
                   name="email"
                 />
                 <span class="absolute start-0 inset-y-0 flex items-center px-2">
@@ -90,16 +199,18 @@ const togglePasswordVisibility = () => {
                 </span>
               </div>
             </FormControl>
+            <FormMessage />
           </FormItem>
         </FormField>
         <FormField v-slot="{ componentField }" name="password">
-          <FormItem class="mt-2 w-11/12">
+          <FormItem v-auto-animate class="w-11/12">
             <div class="flex justify-between items-center self-stretch">
               <FormLabel for="password" class="font-sans text-text text-sm"
                 >Password</FormLabel
               >
-              <div class="flex justify-center items-center gap-x-2">
+              <div class="flex justify-center items-center gap-x-0">
                 <Button
+                  type="button"
                   variant="icon"
                   size="icon"
                   aria-label="Toggle password visibility"
@@ -112,7 +223,6 @@ const togglePasswordVisibility = () => {
                     class="bg-text"
                   />
                 </Button>
-
                 <span class="text-text font-sans text-sm">Hide</span>
               </div>
             </div>
@@ -124,41 +234,65 @@ const togglePasswordVisibility = () => {
                 name="password"
               />
             </FormControl>
+            <FormMessage />
           </FormItem>
         </FormField>
         <FormField
-          v-slot="{ componentField }"
+          v-slot="{ value, handleChange }"
+          v-auto-animate
           name="agreeToTerms"
           type="checkbox"
         >
           <FormItem
-            class="flex items-center p-2 pr-0 self-stretch gap-2 lg:ml-6 3xl:ml-8"
+            v-auto-animate
+            class="flex flex-col items-start p-2 self-stretch gap-2 lg:ml-4 2xl:ml-6 3xl:ml-8"
           >
-            <FormControl>
-              <Checkbox
-                v-bind="componentField"
-                id="agreeToTerms"
-                class="text-primaryColor-400 lg:mt-1"
-                name="agreeToTerms"
+            <div class="flex items-center pl-1 lg:pl-0">
+              <FormControl>
+                <Checkbox
+                  id="agreeToTerms"
+                  type="checkbox"
+                  class="text-primaryColor-400 mr-1"
+                  name="agreeToTerms"
+                  :checked="value"
+                  @update:checked="handleChange"
+                >
+                </Checkbox>
+              </FormControl>
+              <FormDescription
+                class="text-text font-sans text-xs font-normal lg:text-sm"
               >
-              </Checkbox>
-            </FormControl>
-            <FormDescription
-              class="text-text font-sans text-xs font-normal lg:text-sm"
-            >
-              By creating an account, you agree to our
-              <NuxtLink to="/terms" class="underline"
-                >Terms and Conditions</NuxtLink
-              >
-            </FormDescription>
+                By creating an account, you agree to our
+                <NuxtLink to="/terms" class="underline"
+                  >Terms and Conditions</NuxtLink
+                >
+              </FormDescription>
+            </div>
+            <FormMessage class="w-full" />
           </FormItem>
         </FormField>
         <Button
+          v-if="!isSubmitting"
           type="submit"
           :size="buttonSize"
+          :disabled="isSubmitting"
           class="bg-secondaryColor w-11/12 rounded-3xl font-heading text-white"
           >Create an account</Button
         >
+        <Button
+          v-else
+          disabled
+          type="submit"
+          :size="buttonSize"
+          class="bg-secondaryColor w-11/12 rounded-3xl font-heading text-white"
+        >
+          <Icon
+            name="i-lucide-loader-circle"
+            class="animate-spin bg-text-white mr-2"
+            size="1.5rem"
+          />
+          Creating account
+        </Button>
       </form>
     </CardContent>
   </Card>
