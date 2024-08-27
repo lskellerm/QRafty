c956122b09c854ffc947722555d839ae0f335513
Luis Kellermann <lekellerm95@gmail.com>
2024-08-27 04:52:21 +0000

    refactor: Update FastAPI configuration and add custom endpoint id generator OpenAPI spec and generating more readable TS Client

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,7 @@ from typing import Any
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 
 from src.auth.router import auth_routers
 from src.config import settings
@@ -12,10 +13,28 @@ from src.config import settings
 ENVIRONMENT = settings.ENVIRONMENT
 SHOW_DOCS_ENVIRONMENTS = settings.SHOW_DOCS_ENVIRONMENTS
 
-fasapi_config: dict[str, Any] = {
+
+# Create dictionary for basic FastAPI configuration
+fastapi_config: dict[str, Any] = {
     "title": "QRafty API",
     "summary": "API to provide functionality for the QRafty Front-End application",
+    "description": "QRafty API helps to manage and create QR codes, allowing the user to create, edit and delete fully custom QR codes.",
 }
+
+
+def custom_generate_uniq_id(route: APIRoute) -> str:
+    """
+    Custom function to generate unique ids for each endpoint of the application.
+
+    Used for the OpenAPI specs to provide clearer and more readable ids for each endpoint, which will
+    be used when generating TypeScript cleint for the Front-End application.
+
+    Args:
+        route (APIRoute): The APIRoute object
+    Returns:
+        str: Unique string id for the endpoint
+    """
+    return f"{route.tags[0]}-{route.name}"
 
 
 @asynccontextmanager
@@ -32,14 +51,20 @@ async def lifespan(app: FastAPI):
     yield
 
 
+# If the current environment is not in the list of environments where the docs should be shown, hide the swagger docs
 if ENVIRONMENT not in SHOW_DOCS_ENVIRONMENTS:
-    fasapi_config["openapi_url"] = (
+    fastapi_config["openapi_url"] = (
         None  # sets the url for docs as null, that is to hide the swagger docs
     )
 
+# Create the instance of the FastAPI application, with custom lifespan and unique id generator, dumping the configuration
+app = FastAPI(
+    lifespan=lifespan,
+    generate_unique_id_function=custom_generate_uniq_id,
+    **fastapi_config,
+)
 
-app = FastAPI(lifespan=lifespan, **fasapi_config)
-
+# Include the auth routers in the FastAPI application
 for router in auth_routers:
     app.include_router(router, prefix="/auth", tags=["auth"])
 
