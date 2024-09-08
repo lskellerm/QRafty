bb0c81c27c489b213f1c540c5d061101f47e68eb
Luis Kellermann <lekellerm95@gmail.com>
2024-09-08 00:57:13 +0000

    refactor(seo): Update global page meta title tag in app.vue

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | QRafty` : 'QRafty';
+  }
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
