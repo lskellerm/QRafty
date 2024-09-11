282aa1f511db66cbfba65b377759814beeffecbd
Luis Kellermann <lekellerm95@gmail.com>
2024-09-11 02:33:27 +0000

    feat(layouts): Add message.vue layout for displaying confirmation messages

---
+++ frontend/layouts/message.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <!-- Custom centered layout for pages displaying confirmation messages -->
+    <slot />
+  </div>
+</template>
