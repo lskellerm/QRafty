716bda9c13fd39aaa3232b1bcdb527b4f4a6a8b5
Luis Kellermann <lekellerm95@gmail.com>
2024-09-18 21:58:29 +0000

    refactor(app): Set up global registration state for users to track registration status

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,6 +4,16 @@ useHead({
     return titleChunk ? `${titleChunk} | QRafty` : 'QRafty';
   }
 });
+
+/* Set up global registration state for usersto track the registration status across components and sign up worfklow
+   ensuring it is only called once during SSR and client side navigation 
+*/
+
+const isRegistered = useState('registered');
+
+await callOnce(async () => {
+  isRegistered.value = false;
+});
 </script>
 
 <template>
