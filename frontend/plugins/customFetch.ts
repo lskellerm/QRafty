ba0bba282aad375df67799ea5f93665b3190ace5
Luis Kellermann <lekellerm95@gmail.com>
2024-10-24 00:34:45 +0000

    refactor(customPlugin): Refactor customFetch.ts to dynamically set the base API URL based on the environment

--- frontend/plugins/customFetch.ts
+++ frontend/plugins/customFetch.ts
@@ -1,8 +1,11 @@
 import type { $Fetch } from 'nitropack';
 // Create a custom Nuxt Plugin to provide a custom $fetch instance.
 export default defineNuxtPlugin(() => {
-  // Extract base API url from the runtime config
-  const apiBaseUrl = useRuntimeConfig().public.apiBase;
+  // Extract base API url from the runtime config, determined by the environment
+  const apiBaseUrl =
+    useRuntimeConfig().public.environment === 'testing'
+      ? useRuntimeConfig().public.playwrightApiBaseUrl
+      : useRuntimeConfig().public.apiBase;
 
   // Create a custom #fetch instance which wraps the default $fetch, adding options for to the custom QRafty API client
   const api: $Fetch = $fetch.create({
